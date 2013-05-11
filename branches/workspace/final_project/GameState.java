668a920751e9c0b8c28ee0f704d6565a1a64c53c
akshara.bala.18 <akshara.bala.18@2a3c1557-c8bb-04b7-6032-adf11d9d5236>
2013-05-11 04:34:46 +0000

    updated some stuff

--- branches/workspace/final_project/GameState.java
+++ branches/workspace/final_project/GameState.java
@@ -2,13 +2,23 @@ package final_project;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeMap;
+import java.util.Stack;
 
 public class GameState {
+
 	private ArrayList<List<Card>> hands;
-	private ArrayList<Card> deck;
+
+	private Stack<Card> deck;
+
 	private List<Card> discardPile;
+
 	private int currPlayer;
+
 	private int numClues, numLives;
+
 	private List<boolean[]> cardClues;
+
+	public void dealCard(int playerNum) {
+		hands.get(playerNum).add(deck.pop());
+	}
 }
