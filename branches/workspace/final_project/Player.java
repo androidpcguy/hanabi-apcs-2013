fb6a0ee7beb4cb8ceae016f10978486f6611d502
akshara.bala.18 <akshara.bala.18@2a3c1557-c8bb-04b7-6032-adf11d9d5236>
2013-05-13 22:20:38 +0000

    updated some classes (gamestate, player, Server)

--- branches/workspace/final_project/Player.java
+++ branches/workspace/final_project/Player.java
@@ -1,16 +1,27 @@
 package final_project;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
 
 public class Player {
+
 	private ObjectInputStream inputStream;
+
 	private ObjectOutputStream outputStream;
+
+	private Socket socket;
 
 	private GameComponent gameComp;
 
 	private int playerNum;
+
+	public Player(int playerNum, int portNumber, String serverIP) {
+		try {
+			socket = new Socket(serverIP, portNumber);
+		} catch (IOException ioex) {
+			ioex.printStackTrace();
+		}
+	}
 
 	public int getPlayerNum() {
 		return playerNum;
