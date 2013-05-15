7724b51f1f4e9f52e0dfeb9126b817a924acbbf5
akshara.bala.18 <akshara.bala.18@2a3c1557-c8bb-04b7-6032-adf11d9d5236>
2013-05-15 22:01:22 +0000

    updated bunch of code. fixed a lot of TODOs

--- branches/workspace/final_project/Card.java
+++ branches/workspace/final_project/Card.java
@@ -8,10 +8,22 @@ public class Card {
 
 	private int cardIndex;
 
+	private boolean[] colorClues, numberClues;
+
 	public Card(int number, CardColor color, int cardIndex) {
 		this.number = number;
 		this.color = color;
 		this.cardIndex = cardIndex;
+		this.colorClues = new boolean[6];
+		this.numberClues = new boolean[5];
+		initClueArrays();
+	}
+
+	private void initClueArrays() {
+		for (int i = 0; i < 6; i++)
+			colorClues[i] = true;
+		for (int i = 0; i < 5; i++)
+			numberClues[i] = true;
 	}
 
 	public int getNumber() {
@@ -24,6 +36,43 @@ public class Card {
 
 	public int getCardIndex() {
 		return cardIndex;
+	}
+
+	public void giveNumberClue(int number) {
+		if (this.number != number) {
+			this.numberClues[number - 1] = false;
+		} else {
+			for (int i = 0; i < numberClues.length; i++)
+				if (i != (number - 1))
+					numberClues[i] = false;
+		}
+	}
+
+	public void giveColorClue(CardColor color) {
+		int oneTrue = oneColor();
+		if (oneTrue == -1) {
+			if (this.color != color)
+				this.colorClues[color.getIndex()] = false;
+			else
+				for (int i = 0; i < colorClues.length - 1; i++)
+					if (i != color.getIndex())
+						colorClues[i] = false;
+		} else {
+			// TODO: finish this
+		}
+
+	}
+
+	private int oneColor() {
+		// change name this later
+		int i = -1;
+		for (int j = 0; j < colorClues.length - 1; j++)
+			if (colorClues[j] && i != -1)
+				return -1;
+			else if (colorClues[j])
+				i = j;
+		return i;
+
 	}
 
 	public boolean equals(Object other) {
--- branches/workspace/final_project/GameComponent.java
+++ branches/workspace/final_project/GameComponent.java
@@ -1,14 +1,20 @@
 package final_project;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.*;
 
 //this will be a jframe one per player
-public class GameComponent extends JFrame {
+public class GameComponent extends JFrame implements MouseListener {
 
 	private GameState gameState;
 
-	public GameComponent(GameState gameState) {
+	private int playerNum;
+
+	public GameComponent(int playerNum, GameState gameState) {
 		this.gameState = gameState;
+		this.playerNum = playerNum;
 		redraw();
 	}
 
@@ -22,11 +28,32 @@ public class GameComponent extends JFrame {
 	}
 
 	public void play() {
+		
 		// TODO: let the character do something and updates gameState, also need
 		// to redraw after, remember to gameState.currPlayer++
+		gameState.updatePlayer();
+	}
+
+	public boolean doneWithTurn() {
+		return false;// TODO: fix this
 	}
 
 	public void redraw() {
 		// TODO: draw screen with info from gameState
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {}
 }
