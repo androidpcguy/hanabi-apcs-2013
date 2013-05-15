9913d75468878d04e24a03c3916a0658748a70fb
hwalter42@gmail.com <hwalter42@gmail.com@2a3c1557-c8bb-04b7-6032-adf11d9d5236>
2013-05-15 23:24:01 +0000

    Changed GameComponent to a JComponent and started adding new features like paint() and suchforth. Not much changed yet.

--- final_project/GameComponent.java
+++ final_project/GameComponent.java
@@ -1,21 +1,23 @@
 package final_project;
 
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.*;
 
 //this will be a jframe one per player
-public class GameComponent extends JFrame implements MouseListener {
+public class GameComponent extends JComponent implements MouseListener {
 
 	private GameState gameState;
 
 	private int playerNum;
+	
+	private boolean myTurn = false;
 
 	public GameComponent(int playerNum, GameState gameState) {
 		this.gameState = gameState;
 		this.playerNum = playerNum;
-		redraw();
 	}
 
 	public GameState getGameState() {
@@ -24,22 +26,23 @@ public class GameComponent extends JFrame implements MouseListener {
 
 	public void updateGame(GameState gameState) {
 		this.gameState = gameState;
-		// TODO: redraw new gameState
+		repaint();
 	}
 
 	public void play() {
-		
+		myTurn = true;
 		// TODO: let the character do something and updates gameState, also need
 		// to redraw after, remember to gameState.currPlayer++
 		gameState.updatePlayer();
 	}
 
 	public boolean doneWithTurn() {
-		return false;// TODO: fix this
+		return !myTurn;
 	}
-
-	public void redraw() {
-		// TODO: draw screen with info from gameState
+	
+	@Override
+	public void paint (Graphics g) {
+		//TODO paint
 	}
 
 	@Override
